081ced6fcffb01b0e0b9bbecb3e6691a855d2727
jptivan53 <jptivan53@gmail.com>
2019-05-19 04:01:49 -0400

    aasda

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,13 +1,13 @@
 @extends("layouts.app")
 
 @section("content")
-<div class="container rounded mh-100 w-100 bg-light">
+<div class="container bg-light">
     <div class="row align-content-center">
         <div class="col-md-7">
             <h1 class="mt-3 mb-2 title">Productos</h1>
         </div>
         <div class="col-md-5 ml-auto">
-            <form action="{{route('products')}}" method="GET" class="form-inline">
+            <form action="{{route('products.index')}}" method="GET" class="form-inline">
                 <input type="text" name="s" class="form-control mt-3 mb-2 mx-2" placeholder="Nombre del Producto...">
                 <button class="btn btn-primary mt-3 mb-2" type="submit">
                     Buscar <i class="fas fa-search"></i>
@@ -16,26 +16,32 @@
         </div>
     </div>
     <hr>
-    <div class="d-flex flex-row flex-wrap" >
+    <div class="d-flex flex-row flex-wrap mb-3">
         @foreach($products as $product)
             <div class="card shadow-sm my-3 mx-2" style="width: 30%;">
                 <img src="{{ $product->images()->first()->path }}" class="card-img-top">
                 <div class="card-body">
-                    <a href="#" class="card-link">
+                    <a href="{{ route('products.show', ['product' => $product->id])}}" class="card-link">
                         <h4 class="card-title">{{ $product->name }}</h4>
                     </a>
-                    <p class="card-text">{{ $product->description}}</p>
+                    <p class="card-text">
+                        {{ substr($product->description, 0, 200)}}...
+                    </p>
                     <h5>Precio: <span class="badge badge-dark p-2">{{ $product->price }} $</span></h5>
                 </div>
                 <div class="card-footer">
                     @if(!$product->in_stock)
                         <button class="btn btn-secondary disable float-right" disabled>No disponible</button>
                     @else
-                        <button class="btn btn-warning float-right add" data-id="{{ $product->id}}">Añadir al carrito</button>
+                        <button class="btn btn-warning float-right add" data-id="{{ $product->id}}" {{Auth::check()?:'disabled'}}>
+                            Añadir al carrito <i class="fas fa-cart-plus"></i>
+                        </button>
                     @endif
                 </div>
             </div>
-        @endforeach
+        @endforeach  
+    </div>
+    <div class="container">
         {{$products->links()}}
     </div>
 </div>
